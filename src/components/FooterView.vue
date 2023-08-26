02eb7db74365af582f955f807218776bedfe7d33
Amir <amirhkarimi888@gmail.com>
2023-08-26 15:36:32 +0330

    Footer was edited

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -5,7 +5,7 @@
   <div class="footer mt-[250px] z-20 w-full p-4 bg-white border-t border-gray-400 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-900 dark:border-gray-600">
     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
       >© 2023
-      <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All
+      <a href="/" class="hover:underline">AmirHK888™</a>. All
       Rights Reserved.
     </span>
     <ul
--- src/components/HeaderView.vue
+++ src/components/HeaderView.vue
@@ -15,7 +15,7 @@ const props = defineProps(["store"]);
         class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
       >
         <a href="/" class="flex items-center">
-          <img src="favicon.ico" class="h-8 mr-3" alt="Flowbite Logo" />
+          <img src="vue-logo.png" class="h-8 mr-3" alt="Logo" />
           <span
             class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
             >My Website</span
